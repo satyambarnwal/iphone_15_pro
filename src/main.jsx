0dd72444da13f242c97ea23c8a0b521eba3a4cce
Satyam Barnwal <satyambarnwal2002@gmail.com>
2025-01-19 01:45:23 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://d98f1fe5a56057b4c01f35e4f48ad43a@o4508076686966784.ingest.de.sentry.io/4508665414156368",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
